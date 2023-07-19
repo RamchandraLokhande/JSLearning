89ca6d45a880b66baa03887ca7ec083ced0b1c03
RamchandraLokhande <ramchandralokhande88@gmail.com>
2023-07-19 21:05:36 +0530

    Variable Assignment
    Variable information and Variable Assignment

---
+++ 01_variableAssign.js
@@ -0,0 +1,21 @@
+var myFullName;
+console.log(myFullName);
+var myFullName = "Ramchandra Lokhande";
+console.log(myFullName);
+var myFullName = "Ramchandra Dnyanu Lokhande";
+console.log(myFullName);
+var myLove = "I love only JS";
+console.log(myLove);
+var firstName = "Ramchandra"
+console.log("My First Name - ", firstName);
+var lastName = "Lokhande"
+console.log("My Last Name -", lastName);
+var age = "25"
+console.log("Age - ", age);
+var address = "Sangola"
+console.log("Address -", address);
+var pinCode = "PinCode - 413309"
+var gender = "Male"
+console.log("Gender -", gender);
+var isMarried = "True"
+console.log("Marriage Status - ", isMarried);
---
+++ 02_typeOfAssign.js
@@ -0,0 +1,17 @@
+var bankName ;
+console.log(bankName);
+var bankName = 'SBI Bank'
+console.log("Bank Name - ", bankName);
+var marks = '90%'
+console.log("Marks - ", marks);
+console.log("Type of Variable Marks :", typeof marks);
+var isWorking = true;
+console.log("Are you working - ", isWorking);
+console.log("Type of variable isWorking :", typeof isWorking);
+var totalCount = 'Hundred and Seven';
+console.log("Total Count - ", totalCount);
+console.log("Type of variable Total Count : ");
+var totalCount = 107;
+console.log("Total Count - ", totalCount);
+console.log("Type of variable Total Count : ", typeof totalCount);
+
